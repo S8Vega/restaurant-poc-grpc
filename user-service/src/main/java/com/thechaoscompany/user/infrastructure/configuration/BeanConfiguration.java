bdd27fd4bff62a41c2066847efd6d025a2a851ad
bsvega <bsvega@bancolombia.com.co>
2023-02-19 14:53:44 -0500

    add bean configuration of UserServicePort
    Signed-off-by: bsvega <bsvega@bancolombia.com.co>

--- user-service/src/main/java/com/thechaoscompany/user/infrastructure/configuration/BeanConfiguration.java
+++ user-service/src/main/java/com/thechaoscompany/user/infrastructure/configuration/BeanConfiguration.java
@@ -1,8 +1,11 @@
 package com.thechaoscompany.user.infrastructure.configuration;
 
 import com.thechaoscompany.user.domain.api.IObjectServicePort;
+import com.thechaoscompany.user.domain.api.UserServicePort;
 import com.thechaoscompany.user.domain.spi.IObjectPersistencePort;
+import com.thechaoscompany.user.domain.spi.UserPersistensePort;
 import com.thechaoscompany.user.domain.usecase.ObjectUseCase;
+import com.thechaoscompany.user.domain.usecase.UserUseCase;
 import com.thechaoscompany.user.infrastructure.out.jpa.adapter.ObjectJpaAdapter;
 import com.thechaoscompany.user.infrastructure.out.jpa.mapper.IObjectEntityMapper;
 import com.thechaoscompany.user.infrastructure.out.jpa.repository.IObjectRepository;
@@ -24,5 +27,10 @@ public class BeanConfiguration {
     @Bean
     public IObjectServicePort objectServicePort() {
         return new ObjectUseCase(objectPersistencePort());
+    }
+
+    @Bean
+    public UserServicePort userServicePort(UserPersistensePort userPersistensePort) {
+        return new UserUseCase(userPersistensePort);
     }
 }
