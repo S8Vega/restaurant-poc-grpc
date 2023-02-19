8aac731e0bc6acea6ff5b1e82596bb3905ade413
bsvega <bsvega@bancolombia.com.co>
2023-02-19 14:54:02 -0500

    add Autowireds on UserJpaAdapter
    Signed-off-by: bsvega <bsvega@bancolombia.com.co>

--- user-service/src/main/java/com/thechaoscompany/user/infrastructure/out/jpa/adapter/UserJpaAdapter.java
+++ user-service/src/main/java/com/thechaoscompany/user/infrastructure/out/jpa/adapter/UserJpaAdapter.java
@@ -5,11 +5,14 @@ import com.thechaoscompany.user.domain.spi.UserPersistensePort;
 import com.thechaoscompany.user.infrastructure.out.jpa.entity.UserEntity;
 import com.thechaoscompany.user.infrastructure.out.jpa.mapper.UserEntityMapper;
 import com.thechaoscompany.user.infrastructure.out.jpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserJpaAdapter implements UserPersistensePort {
+    @Autowired
     private UserRepository repository;
+    @Autowired
     private UserEntityMapper mapper;
 
     @Override
