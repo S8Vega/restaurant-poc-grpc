96f7b5d34137c6eb42907a4056f49a1f0ec0bc94
bsvega <bsvega@bancolombia.com.co>
2023-02-19 14:10:33 -0500

    add UserHandler
    Signed-off-by: bsvega <bsvega@bancolombia.com.co>

---
+++ user-service/src/main/java/com/thechaoscompany/user/infrastructure/input/rest/UserRestController.java
@@ -0,0 +1,34 @@
+package com.thechaoscompany.user.infrastructure.input.rest;
+
+import com.thechaoscompany.user.application.dto.request.UserRequestDto;
+import com.thechaoscompany.user.application.handler.UserHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserRestController {
+
+    private final UserHandler handler;
+
+    @Operation(summary = "Add a new User")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created", content = @Content),
+            @ApiResponse(responseCode = "409", description = "User already exists", content = @Content)
+    })
+    @PostMapping("/")
+    public ResponseEntity<Void> save(@RequestBody UserRequestDto request) {
+        handler.save(request);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
