6e8e08ee32ecf2368a0f824d9dca85e08356919e
bsvega <bsvega@bancolombia.com.co>
2023-02-19 09:16:14 -0500

    change generated value on id of UserEntity
    Signed-off-by: bsvega <bsvega@bancolombia.com.co>

--- user-service/src/main/java/com/thechaoscompany/user/infrastructure/out/jpa/entity/UserEntity.java
+++ user-service/src/main/java/com/thechaoscompany/user/infrastructure/out/jpa/entity/UserEntity.java
@@ -4,8 +4,6 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -19,7 +17,6 @@ public class UserEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "nombre")
     private String name;
