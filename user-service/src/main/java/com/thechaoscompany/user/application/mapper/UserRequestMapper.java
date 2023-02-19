6fd51f377562af1d189e3b40701ed2dad45ad028
bsvega <bsvega@bancolombia.com.co>
2023-02-19 09:55:03 -0500

    add UserRequestMapper
    Signed-off-by: bsvega <bsvega@bancolombia.com.co>

---
+++ user-service/src/main/java/com/thechaoscompany/user/application/mapper/UserRequestMapper.java
@@ -0,0 +1,13 @@
+package com.thechaoscompany.user.application.mapper;
+
+import com.thechaoscompany.user.application.dto.request.UserRequestDto;
+import com.thechaoscompany.user.domain.model.UserModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface UserRequestMapper {
+    UserModel toModel(UserRequestDto user);
+}
