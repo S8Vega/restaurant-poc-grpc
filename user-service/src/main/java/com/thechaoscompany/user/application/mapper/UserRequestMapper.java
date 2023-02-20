e080c74d5814e092e8ee761a1daff797c24716b4
bsvega <bsvega@bancolombia.com.co>
2023-02-19 22:26:06 -0500

    fix mapper for map role name and description
    Signed-off-by: bsvega <bsvega@bancolombia.com.co>

--- user-service/src/main/java/com/thechaoscompany/user/application/mapper/UserRequestMapper.java
+++ user-service/src/main/java/com/thechaoscompany/user/application/mapper/UserRequestMapper.java
@@ -3,11 +3,14 @@ package com.thechaoscompany.user.application.mapper;
 import com.thechaoscompany.user.application.dto.request.UserRequestDto;
 import com.thechaoscompany.user.domain.model.UserModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface UserRequestMapper {
+    @Mapping(target = "role.name", source = "user.nameRole")
+    @Mapping(target = "role.description", source = "user.descriptionRole")
     UserModel toModel(UserRequestDto user);
 }
